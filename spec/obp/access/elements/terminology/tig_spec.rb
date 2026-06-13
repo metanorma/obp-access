8c98e4a8eb5064993ec6024c42ffd8cf22cf5134
Ronald Tse <ronald.tse@ribose.com>
2026-06-13 18:51:43 +0800

    Fix grammatical gender to use NISO STS conformant element and values
    obp-access was emitting <tbx:gram value="f" type="gender"/> which is
    not a valid NISO STS element. The NISO STS TBX schema defines
    <tbx:grammaticalGender> with values masculine/feminine/neuter.
    
    Changes:
    - GrammarParser: GENDER_MAP maps HTML short codes (m/f/n) to full-word
      NISO STS values (masculine/feminine/neuter). All gender handlers now
      route through GENDER_MAP so Result.genders always carries conformant
      values.
    - Tig#render_genders: emits <tbx:grammaticalGender> instead of
      <tbx:gram type="gender">.
    - Updated grammar_parser_spec and tig_spec with gender rendering specs.
    - Updated README to document the correct element name and values.

--- spec/obp/access/elements/terminology/tig_spec.rb
+++ spec/obp/access/elements/terminology/tig_spec.rb
@@ -5,7 +5,8 @@ require "spec_helper"
 RSpec.describe Obp::Access::Renderer::Elements::Terminology::Tig do
   def build_document
     Nokogiri::XML(<<~XML)
-      <standard xmlns:xlink="http://www.w3.org/1999/xlink">
+      <standard xmlns:tbx="urn:iso:std:iso:30042:ed-2"
+                xmlns:xlink="http://www.w3.org/1999/xlink">
         <body>
           <tbx:termEntry id="term_3.1.1">
             <tbx:langSet xml:lang="en"/>
@@ -16,11 +17,13 @@ RSpec.describe Obp::Access::Renderer::Elements::Terminology::Tig do
   end
 
   def build_node(inner_html)
-    doc = Nokogiri::HTML.fragment(<<~HTML)
-      <div class="sts-tbx-sec">
-        <div class="sts-tbx-label">3.1.1</div>
-        <div class="sts-tbx-term">#{inner_html}</div>
-      </div>
+    doc = Nokogiri::HTML::Document.parse(<<~HTML)
+      <html><body>
+        <div class="sts-tbx-sec">
+          <div class="sts-tbx-label">3.1.1</div>
+          <div class="sts-tbx-term">#{inner_html}</div>
+        </div>
+      </body></html>
     HTML
     doc.at_css("div.sts-tbx-term")
   end
@@ -39,6 +42,53 @@ RSpec.describe Obp::Access::Renderer::Elements::Terminology::Tig do
       node = build_node("pump")
       element = described_class.new(document: document, metas: metas, node: node)
       expect(element.match_node?).to be true
+    end
+  end
+
+  describe "grammaticalGender rendering" do
+    let(:gender_test_class) do
+      Class.new(described_class) do
+        def build_gender_xml
+          result = Obp::Access::GrammarParser.parse(node.inner_html)
+          Nokogiri::XML::Builder.new do |xml|
+            xml.root do
+              render_genders(xml, result.genders)
+            end
+          end.doc.root.to_xml
+        end
+      end
+    end
+
+    def rendered_genders(inner_html)
+      node = build_node(inner_html)
+      element = gender_test_class.new(document: document, metas: metas, node: node)
+      element.build_gender_xml
+    end
+
+    it "emits tbx:grammaticalGender for a feminine term" do
+      expect(rendered_genders("<b>f</b> pompe")).to include('value="feminine"')
+    end
+
+    it "emits tbx:grammaticalGender for a masculine term" do
+      expect(rendered_genders("<b>m</b> Kolben")).to include('value="masculine"')
+    end
+
+    it "emits tbx:grammaticalGender for a neuter term" do
+      expect(rendered_genders("<b>n</b> Ventil")).to include('value="neuter"')
+    end
+
+    it "emits multiple tbx:grammaticalGender elements for multi-gender terms" do
+      xml = rendered_genders("<b>m, n</b> Gehäuse")
+      expect(xml.scan("grammaticalGender").length).to eq(2)
+    end
+
+    it "does not emit the legacy tbx:gram element" do
+      xml = rendered_genders("<b>f</b> pompe")
+      expect(xml).not_to match(/tbx:gram\b(?!aticalGender)/)
+    end
+
+    it "omits grammaticalGender when no gender marker is present" do
+      expect(rendered_genders("pump")).not_to include("grammaticalGender")
     end
   end
 end
